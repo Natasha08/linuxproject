37d54088ab4fc5bdb0e8dce6b7c596dd5f49d826
Natasha Osborne <natasha08@me.com>
2016-04-21 17:23:24 -0600

    new app install for to generate an ejs template and fix other VMachine issues

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+// Project configuration. 
+grunt.initConfig({
+  concat: {
+    js: {
+      src: ['app.js', 'routes/*.js', 'public/javascripts/*.js'],
+      dest: 'build/scripts.js',
+    },
+    css: {
+      src: ['public/stylesheets/style.css', 'public/stylesheets/header.css', 'public/stylesheets/footer.css', 'public/stylesheets/login.css'],
+      dest: 'build/styles.css',
+    },    
+  },
+
+watch: {
+  css: {
+    files: ['public/stylesheets/style.css', 'public/stylesheets/header.css', 'public/stylesheets/footer.css', 'public/stylesheets/login.css'],
+    tasks: ['concat'],
+  },
+  js: {
+    files: ['app.js', 'routes/*.js', 'public/javascripts/*.js'],
+    tasks: ['concat'],
+  },  
+},
+
+ uglify: {
+    my_target: {
+      files: {
+        'build/output.min.js': ['build/scripts.js'],
+      }
+    }
+  }
+
+});
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+
+};
