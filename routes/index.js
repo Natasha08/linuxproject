88edc10922d9e7cbc84f8797aa511b9a1259ce84
Natasha Osborne <natasha08@me.com>
2016-05-12 16:04:25 -0600

    updated .gitignore. Added sendgrid to package json, successfully test plain text email with npm sendgrid docs

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var dbrequire = require('../public/javascripts/require');
+// var sendgrid_api = require('../secret');
+// var sendgrid  = require('sendgrid')(sendgrid_api);
 
 router.get('/', function(req, res, next) {
+
+//  sendgrid.send({
+//   to:       '',
+//   from:     '',
+//   subject:  '',
+//   text:     ''
+// }, function(err, json) {
+//   if (err) { return console.error(err); }
+//   console.log(json);
+// });
+
 
 	res.render('index', {title: 'MyColo'});
 
