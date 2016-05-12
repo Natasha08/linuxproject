7bbd40804752121a25c5e1a48136e8a6eb30108b
Natasha Osborne <natasha08@me.com>
2016-05-12 12:06:15 -0600

    added grunt dep - cssmin, created output.min.css, loaded task to registerTask

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ concat: {
     dest: 'build/scripts.js',
   },
   css: {
-    src: ['public/stylesheets/*.css'],
+    src: ['public/stylesheets/footer.css', 'public/stylesheets/style.css'],
     dest: 'build/styles.css',
   },    
 },
@@ -45,27 +45,35 @@ watch: {
     my_target: {
       files: {
         'build/output.min.js': ['build/scripts.js'],
-      }
+      },
+    },
+  },
+  cssmin: {
+  target: {
+    files: {
+      'build/output.min.css': ['build/styles.css'],
     }
   }
+}
 
 });
 
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 //select and uncomment correct concat concat above per task
 //production grunt default 
-grunt.registerTask('default', ['concat', 'uglify']);
+grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 //dev build
-grunt.registerTask('dev', ['concat', 'uglify', 'watch']);
+grunt.registerTask('dev', ['concat', 'uglify','cssmin', 'watch']);
 
 
 //working on including this as a third command. Looking for alternative to comment/uncomment solution above
 //git grunt default
-grunt.registerTask('git', ['concat', 'uglify']);
+grunt.registerTask('git', ['concat', 'uglify', 'cssmin']);
 };
 
 
